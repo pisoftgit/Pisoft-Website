d35f8d7154dca2936a4931788a3ae8f3921ce10c
pisoftgit <pisoftgit@gmail.com>
2025-07-21 15:37:59 +0530

    buttons

--- src/components/AuthFloatingButtons.jsx
+++ src/components/AuthFloatingButtons.jsx
@@ -5,10 +5,10 @@ const AuthFloatingButtons = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="fixed top-2 right-6 z-[1000] flex flex-row gap-4 bg-orange-100 border border-gray-300 rounded-full shadow-md p-2">
+    <div className="fixed top-2 right-2 z-[1000] flex flex-row bg-orange-300 border border-gray-300 rounded-full shadow-md p-2">
       <button
         onClick={() => navigate("/login")}
-        className="relative text-blue-900 font-jr px-6 py-2 rounded-full hover:bg-sky-100 transition"
+        className="relative text-blue-900 font-jr px-4 py-2 rounded-full hover:bg-sky-100 transition"
       >
         Login
         <span className="absolute inset-0 rounded-full -z-10" />
@@ -16,7 +16,7 @@ const AuthFloatingButtons = () => {
 
       <button
         onClick={() => navigate("/register")}
-        className="relative text-orange-600 font-jr px-6 py-2 rounded-full hover:bg-sky-100 transition"
+        className="relative text-blue-900 font-jr px-4 py-2 rounded-full hover:bg-sky-100 transition"
       >
         Register
         <span className="absolute inset-0 rounded-full -z-10" />
